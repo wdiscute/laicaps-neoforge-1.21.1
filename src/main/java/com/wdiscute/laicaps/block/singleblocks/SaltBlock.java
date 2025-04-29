4f8bd02c8145c03f41438c97e8fd0452dcc3cb3c
wd <lucasalvesbispo2@gmail.com>
2025-04-29 18:32:24 +0100

    more work on telescope UI

--- src/main/java/com/wdiscute/laicaps/block/singleblocks/SaltBlock.java
+++ src/main/java/com/wdiscute/laicaps/block/singleblocks/SaltBlock.java
@@ -1,6 +1,7 @@
 package com.wdiscute.laicaps.block.singleblocks;
 
 import com.mojang.serialization.MapCodec;
+import com.wdiscute.laicaps.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
@@ -9,6 +10,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RedStoneWireBlock;
@@ -22,6 +25,7 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
 
 public class SaltBlock extends Block
 {
@@ -37,9 +41,36 @@ public class SaltBlock extends Block
 
 
     @Override
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean movedByPiston)
+    {
+        BlockState bs = state;
+
+        bs = bs.setValue(NORTH, (level.getBlockState(pos.north()).is(ModBlocks.SALT)) ? RedstoneSide.SIDE :RedstoneSide.NONE);
+        bs = bs.setValue(SOUTH, (level.getBlockState(pos.south()).is(ModBlocks.SALT)) ? RedstoneSide.SIDE :RedstoneSide.NONE);
+        bs = bs.setValue(EAST, (level.getBlockState(pos.east()).is(ModBlocks.SALT)) ? RedstoneSide.SIDE :RedstoneSide.NONE);
+        bs = bs.setValue(WEST, (level.getBlockState(pos.west()).is(ModBlocks.SALT)) ? RedstoneSide.SIDE :RedstoneSide.NONE);
+
+        level.setBlockAndUpdate(pos, bs);
+
+        super.neighborChanged(state, level, pos, neighborBlock, neighborPos, movedByPiston);
+    }
+
+
+    @Override
     protected MapCodec<? extends HorizontalDirectionalBlock> codec()
     {
         return null;
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context)
+    {
+        BlockState bs = ModBlocks.SALT.get().defaultBlockState();
+        bs = bs.setValue(NORTH, (context.getLevel().getBlockState(context.getClickedPos().north()).is(ModBlocks.SALT)) ? RedstoneSide.SIDE :RedstoneSide.NONE);
+        bs = bs.setValue(SOUTH, (context.getLevel().getBlockState(context.getClickedPos().south()).is(ModBlocks.SALT)) ? RedstoneSide.SIDE :RedstoneSide.NONE);
+        bs = bs.setValue(EAST, (context.getLevel().getBlockState(context.getClickedPos().east()).is(ModBlocks.SALT)) ? RedstoneSide.SIDE :RedstoneSide.NONE);
+        bs = bs.setValue(WEST, (context.getLevel().getBlockState(context.getClickedPos().west()).is(ModBlocks.SALT)) ? RedstoneSide.SIDE :RedstoneSide.NONE);
+        return bs;
     }
 
     @Override
