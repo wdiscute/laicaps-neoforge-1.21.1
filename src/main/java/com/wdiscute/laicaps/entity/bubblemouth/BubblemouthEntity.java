199f53862287778c4139377d3fa782c2167cfa9c
wd <lucasalvesbispo2@gmail.com>
2025-05-26 13:18:28 +0100

    added bubble particles to bubblemouth

--- src/main/java/com/wdiscute/laicaps/entity/bubblemouth/BubblemouthEntity.java
+++ src/main/java/com/wdiscute/laicaps/entity/bubblemouth/BubblemouthEntity.java
@@ -1,6 +1,7 @@
 package com.wdiscute.laicaps.entity.bubblemouth;
 
 import com.wdiscute.laicaps.ModItems;
+import com.wdiscute.laicaps.particle.ModParticles;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
@@ -12,53 +13,79 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.AbstractFish;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.joml.Vector2d;
 
 public class BubblemouthEntity extends AbstractFish
 {
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
 
-    public BubblemouthEntity(EntityType<? extends BubblemouthEntity> entityType, Level level) {
+    public BubblemouthEntity(EntityType<? extends BubblemouthEntity> entityType, Level level)
+    {
         super(entityType, level);
     }
 
     @Override
-    protected SoundEvent getAmbientSound() {
+    protected SoundEvent getAmbientSound()
+    {
         return SoundEvents.TROPICAL_FISH_AMBIENT;
     }
 
     @Override
-    protected SoundEvent getDeathSound() {
+    protected SoundEvent getDeathSound()
+    {
         return SoundEvents.TROPICAL_FISH_DEATH;
     }
 
     @Override
-    protected SoundEvent getHurtSound(DamageSource damageSource) {
+    protected SoundEvent getHurtSound(DamageSource damageSource)
+    {
         return SoundEvents.TROPICAL_FISH_HURT;
     }
 
     @Override
-    protected SoundEvent getFlopSound() {return SoundEvents.TROPICAL_FISH_FLOP;}
+    protected SoundEvent getFlopSound()
+    {
+        return SoundEvents.TROPICAL_FISH_FLOP;
+    }
 
-    public static AttributeSupplier.Builder createAttributes() {
+    public static AttributeSupplier.Builder createAttributes()
+    {
         return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 5.0F);
     }
 
-    private void setupAnimationStates() {
-        if(this.idleAnimationTimeout <= 0) {
+    private void setupAnimationStates()
+    {
+        if (this.idleAnimationTimeout <= 0)
+        {
             this.idleAnimationTimeout = 80;
             this.idleAnimationState.start(this.tickCount);
-        } else {
+        }
+        else
+        {
             --this.idleAnimationTimeout;
         }
     }
 
     @Override
-    public void tick() {
+    public void tick()
+    {
         super.tick();
 
-        if(this.level().isClientSide()) {
+        if (level().isClientSide)
+        {
             this.setupAnimationStates();
+            if (getRandom().nextFloat() > 0.95)
+            {
+                level().addParticle(
+                        ModParticles.CHASE_PUZZLE_PARTICLES.get(),
+                        getX() + (getLookAngle().x * 0.5),
+                        getY() + 0.2,
+                        getZ() + (getLookAngle().z * 0.5),
+                        0,
+                        0,
+                        0);
+            }
         }
     }
 
