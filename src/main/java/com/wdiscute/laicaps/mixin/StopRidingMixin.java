383b3e1d0dc6421eea8d34f6f0051135b19ef3b8
wd <lucasalvesbispo2@gmail.com>
2025-06-20 18:14:17 +0100

    added "it is not safe to unmount now" message

--- src/main/java/com/wdiscute/laicaps/mixin/StopRidingMixin.java
+++ src/main/java/com/wdiscute/laicaps/mixin/StopRidingMixin.java
@@ -3,6 +3,7 @@ package com.wdiscute.laicaps.mixin;
 import com.mojang.authlib.GameProfile;
 import com.wdiscute.laicaps.entity.rocket.RocketEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
@@ -16,10 +17,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
-public class StopRidingMixin extends Player
+public abstract class StopRidingMixin extends Player
 {
 
     @Shadow @Final private static Logger LOGGER;
+
+    @Shadow public abstract void displayClientMessage(Component chatComponent, boolean actionBar);
 
     public StopRidingMixin(Level level, BlockPos pos, float yRot, GameProfile gameProfile)
     {
@@ -34,9 +37,12 @@ public class StopRidingMixin extends Player
             System.out.println(re);
             if(re.getEntityData().get(RocketEntity.STATE) != 0)
             {
+                displayClientMessage(Component.translatable("gui.laicaps.rocket.unsafe"), true);
                 ci.cancel();
             }
         }
+
+
     }
 
     @Override
