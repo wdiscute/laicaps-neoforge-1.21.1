211d4ea39e8b99a8bfe2895e80e125bdc4f9dd96
wd <lucasalvesbispo2@gmail.com>
2025-08-29 23:46:59 +0100

    added gradient support

--- src/main/java/com/wdiscute/laicaps/util/Tooltips.java
+++ src/main/java/com/wdiscute/laicaps/util/Tooltips.java
@@ -6,6 +6,7 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
+import org.apache.commons.codec.binary.Hex;
 
 import java.awt.*;
 import java.util.List;
@@ -21,6 +22,7 @@ public class Tooltips
         if (I18n.exists("tooltip." + BuiltInRegistries.ITEM.getKey(stack.getItem()).getNamespace() + "." + BuiltInRegistries.ITEM.getKey(stack.getItem()).getPath() + ".0"))
         {
             Laicaps.hue += 0.001f;
+            System.out.println(Laicaps.hue);
             if (event.getFlags().hasShiftDown())
             {
                 tooltipComponents.add(Component.translatable("tooltip.laicaps.generic.shift_down"));
@@ -33,12 +35,26 @@ public class Tooltips
 
                     String s = I18n.get("tooltip." + BuiltInRegistries.ITEM.getKey(stack.getItem()).getNamespace() + "." + BuiltInRegistries.ITEM.getKey(stack.getItem()).getPath() + "." + i).toString();
 
-                    if(s.contains("<rgb>"))
+                    //s = "<gradient00>A gorgeous Angelfish, one of the last of it's kind.</gradient22>";
+
+
+                    if (s.contains("<rgb>"))
                     {
                         Component c = Component.literal("")
                                 .append(s.substring(0, s.indexOf("<rgb>")))
-                                .append(RGBEachLetter(Laicaps.hue, s.substring(s.indexOf("<rgb>") + 5, s.length() - s.indexOf("</rgb>") - 12), 0.01f))
+                                .append(RGBEachLetter(Laicaps.hue, s.substring(s.indexOf("<rgb>") + 5, s.indexOf("</rgb>")), 0.01f))
                                 .append(s.substring(s.indexOf("</rgb>") + 6));
+                        tooltipComponents.add(c);
+                    }
+                    else if (s.contains("<gradient"))
+                    {
+                        float min = Float.parseFloat("0." + s.substring(s.indexOf("<gradient") + 10, s.indexOf("<gradient") + 12));
+                        float max = Float.parseFloat("0." + s.substring(s.indexOf("</gradient") + 11, s.indexOf("</gradient") + 13));
+
+                        Component c = Component.literal("")
+                                .append(s.substring(0, s.indexOf("<gradient")))
+                                .append(Gradient(Laicaps.hue, s.substring(s.indexOf("<gradient") + 13, s.indexOf("</gradient")), min, max))
+                                .append(s.substring(s.indexOf("</gradient") + 14));
                         tooltipComponents.add(c);
                     }
                     else
@@ -47,15 +63,47 @@ public class Tooltips
                     }
 
 
-
                 }
 
-            } else
+            }
+            else
             {
                 tooltipComponents.add(Component.translatable("tooltip.laicaps.generic.shift_up"));
             }
 
         }
+    }
+
+    public static Component Gradient(float hue, String text, float min, float max)
+    {
+        Component c = Component.literal("");
+
+        for (int i = 0; i < text.length(); i++)
+        {
+            String s = text.substring(i, i + 1);
+
+
+            float pingPongedHue = mapHuePingPong(i * 0.01f + hue, min, max);
+
+            int color = Color.HSBtoRGB(pingPongedHue, 1, 1);
+
+            Component l = Component.literal(s).withColor(color);
+
+            c = Component.literal("").append(c).append(l);
+        }
+
+        return c;
+    }
+
+    public static float mapHuePingPong(float h, float min, float max)
+    {
+        h = h % 1;
+
+        // Triangle wave between 0 and 1
+        float t = Math.abs(2f * h - 1f);
+
+        // Scale to [min,max]
+        return min + t * (max - min);
     }
 
 
@@ -68,7 +116,7 @@ public class Tooltips
 
         for (int i = 0; i < text.length(); i++)
         {
-            String s = text.substring(i, i+1);
+            String s = text.substring(i, i + 1);
 
             int color = Color.HSBtoRGB(i * speed + hue, 1, 1);
 
